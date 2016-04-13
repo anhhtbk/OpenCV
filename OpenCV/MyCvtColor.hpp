cc475397da65c4ab3fa93f502a08be32bc7a25ae
VMio69 <vmio69@vmio-mac.local>
2016-04-13 17:44:19 +0700

    c

--- OpenCV/MyCvtColor.hpp
+++ OpenCV/MyCvtColor.hpp
@@ -38,9 +38,7 @@ namespace myCv {
         void cvtGrayChannel(const Mat &matFrom, Mat &matTo, int color);
         
         void cvtGray(const Mat &matFrom, Mat &matTo, double c1 = 1, double c2 = 1, double c3 = 1);
-        
-//        void cvtGrayAllChanels(const Mat &matFrom, string colorSpace);
-        
+             
         ~MyCvtColor(void);
     };
     
