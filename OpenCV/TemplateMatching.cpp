467335634b0348dd9f928db2bd04804ac38df665
VMio69 <vmio69@vmio-mac.local>
2016-04-15 17:54:26 +0700

    c

--- OpenCV/TemplateMatching.cpp
+++ OpenCV/TemplateMatching.cpp
@@ -27,6 +27,7 @@ int max_Trackbar = 5;
 void MatchingMethod( int, void* );
 
 void rotateWarpImage(Mat &img, double angle, double scale);
+void unRotateWarpImage(Mat &img, double angle, double scale);
 /**
  * @function main
  */
@@ -34,7 +35,7 @@ int main()
 {
     /// Load image and template
     img = imread( "/Users/vmio69/Desktop/OpenCV/1.jpg", 1 );
-    templ = imread( "/Users/vmio69/Desktop/OpenCV/temp.jpg", 1 );
+    templ = imread( "/Users/vmio69/Desktop/OpenCV/temp.png", 1 );
     
     
     /// Create windows
@@ -65,6 +66,9 @@ void rotateWarpImage(Mat &img, double angle, double scale){
     warpAffine(img, img, rot, bbox.size());
 }
 
+void unRotateWarpImage(Mat &img, double angle, double scale){
+    rotateWarpImage(img, -angle, scale/(1+scale));
+}
 
 /**
  * @function MatchingMethod
@@ -72,74 +76,72 @@ void rotateWarpImage(Mat &img, double angle, double scale){
  */
 void MatchingMethod( int, void* )
 {
-    double angle = 0, scale = 1.;
-    double matchAngle = 0.0, matchScale = 0.0;
-    double curMinVal = INT32_MAX, curMaxVal = INT32_MIN;
-    Point matchLoc;
     /// Source image to display
-    Mat img_display, templCopy;
+    Mat img_display;
     img.copyTo( img_display );
-    templ.copyTo(templCopy);
-    Point center = Point(templ.cols/2,templ.rows/2);
-    Mat rot_mat(2,3,CV_32FC1);
-
+    Mat source;
+    Point matchLoc;
+    double curMinVal = INT32_MAX, curMaxVal = INT32_MIN;
+    double matchAngle = 0, matchScale = 1;
     
-//    for (scale = 0.05; scale < 5.; scale += 0.05) {
-
-        
-        /// Create the result matrix
-        int result_cols =  img.cols - templCopy.cols + 1;
-        int result_rows = img.rows - templCopy.rows + 1;
-        
-//        if (result_cols < 0 || result_rows < 0) {
-//            break;
-//        }
-    
-        result.create( result_rows, result_cols, CV_32FC1 );
-        
-        /// Do the Matching and Normalize
-        matchTemplate( img, templCopy, result, match_method );
-        normalize( result, result, 0, 1, NORM_MINMAX, -1, Mat() );
-        
-        /// Localizing the best match with minMaxLoc
-        double minVal; double maxVal; Point minLoc; Point maxLoc;
-        
-        
-        minMaxLoc( result, &minVal, &maxVal, &minLoc, &maxLoc, Mat() );
-        
-        
-        /// For SQDIFF and SQDIFF_NORMED, the best matches are lower values. For all the other methods, the higher the better
-        if( match_method  == TM_SQDIFF || match_method == TM_SQDIFF_NORMED )
-        {
-            if (minVal < curMinVal) {
-                curMinVal = minVal;
-                matchLoc = minLoc;
-                matchAngle = angle;
-                matchScale = scale;
-            }
-        }
-        else
-        {
-            if (maxVal > curMaxVal) {
-                curMaxVal = maxVal;
-                matchLoc = maxLoc;
-                matchAngle = angle;
-                matchScale = scale;
-            }
+    double angle = 0, scale = 1;
+    for (angle = 0; angle < 360; angle+=45) {
+        for (scale = 0.5; scale < 2; scale += 0.5) {
+            img.copyTo(source);
+            rotateWarpImage(source, angle, scale);
             
+            
+            
+            /// Create the result matrix
+            int result_cols =  source.cols - templ.cols + 1;
+            int result_rows = source.rows - templ.rows + 1;
+            
+            if (result_cols <= 0 || result_rows <= 0) {
+                break;
+            }
+    
+            result.create( result_rows, result_cols, CV_32FC1 );
+            
+            /// Do the Matching and Normalize
+            matchTemplate( source, templ, result, match_method );
+            normalize( result, result, 0, 1, NORM_MINMAX, -1, Mat() );
+            
+            /// Localizing the best match with minMaxLoc
+            double minVal; double maxVal; Point minLoc; Point maxLoc;
+        
+            minMaxLoc( result, &minVal, &maxVal, &minLoc, &maxLoc, Mat() );
+            
+            /// For SQDIFF and SQDIFF_NORMED, the best matches are lower values. For all the other methods, the higher the better
+            if( match_method  == CV_TM_SQDIFF || match_method == CV_TM_SQDIFF_NORMED )
+            {
+                if (minVal < curMinVal) {
+                    curMinVal = minVal;
+                    matchLoc = minLoc;
+                    matchAngle = angle;
+                    matchScale = scale;
+                }
+            }
+            else
+            {
+                if (maxVal > curMaxVal) {
+                    curMaxVal = maxVal;
+                    matchLoc = maxLoc;
+                    matchAngle = angle;
+                    matchScale = scale;
+                }
+                
+            }
         }
-//    }
-   
-    resize(templ, templCopy, Size(0,0),matchScale,matchScale);
-    rot_mat = getRotationMatrix2D(center, matchAngle, matchScale);
-//    warpAffine(templ, templCopy, rot_mat, templCopy.size());
+    }
+
+    
     
     /// Show me what you got
-    rectangle( img_display, matchLoc, Point( matchLoc.x + templCopy.cols , matchLoc.y + templCopy.rows ), Scalar::all(0), 2, 8, 0 );
-   // rectangle( result, matchLoc, Point( matchLoc.x + templ.cols , matchLoc.y + templ.rows ), Scalar::all(0), 2, 8, 0 );
+    rectangle( img_display, matchLoc, Point( matchLoc.x + templ.cols , matchLoc.y + templ.rows ), Scalar::all(0), 2, 8, 0 );
+    rectangle( result, matchLoc, Point( matchLoc.x + templ.cols , matchLoc.y + templ.rows ), Scalar::all(0), 2, 8, 0 );
     
     imshow( image_window, img_display );
-   // imshow( result_window, result );
+    imshow( result_window, result );
     
     return;
 }
